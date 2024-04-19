232fef196bdb4f80ed4f18b85753dc8c429d354a
Barendan <barendan@gmail.com>
2024-04-19 08:39:06 -0400

    initial commit

---
+++ kucoin_futures.py
@@ -0,0 +1,17 @@
+import logging
+import requests
+import pprint
+
+logger = logging.getLogger()
+
+
+
+# def get_contracts():
+#
+#     response_object = requests.get('')
+#     print(response_object)
+#     pprint.print(response_object)
+#
+#     pprint.pprint(response_object.json()['symbols'])
+#
+# get_contracts()
