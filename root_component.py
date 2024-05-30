5114548839aa43ecf9a5e878c892880ef8c8ff1c
Barendan <barendan@gmail.com>
2024-05-30 15:29:14 -0400

    start building trade watch comp and add to the ui

--- root_component.py
+++ root_component.py
@@ -4,6 +4,7 @@ import logging
 from styling import *
 from logging_component import Logging
 from watchlist_component import Watchlist
+from trades_component import TradesWatch
 
 logger = logging.getLogger()
 
@@ -28,6 +29,9 @@ class Root(tk.Tk):
 
         self._logging_frame = Logging(self._left_frame, bg=BG_COLOR)
         self._logging_frame.pack(side=tk.TOP)
+
+        self._trades_frame = TradesWatch(self._right_frame, bg=BG_COLOR)
+        self._trades_frame.pack(side=tk.TOP)
 
         self.update_ui()
 
